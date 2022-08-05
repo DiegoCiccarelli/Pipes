e278f75ec96f64c1f58880e2ec6724e10558f5f1
DiegoCiccarelli <ciccarellidiego@gmail.com>
2022-08-05 20:43:26 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = "Diego"
+
+  cambiarNombre(){
+    console.log(this.nombre)
+  }
 }
